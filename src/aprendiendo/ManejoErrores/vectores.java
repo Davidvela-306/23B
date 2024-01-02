249345c1fffd00dfa8146b9b09dcec1d726d868d
Davidvela-306 <92609657+Davidvela-306@users.noreply.github.com>
2024-01-02 09:42:24 -0500

    RefuerzoExamen02-01-2024

--- src/aprendiendo/ManejoErrores/vectores.java
+++ src/aprendiendo/ManejoErrores/vectores.java
@@ -8,12 +8,13 @@ public class vectores {
         System.out.println("Ingrese tamaño arreglo");
         int length;
         length = myObj.nextInt();
-        double num[length];
+        double num[] = new double[length];
 
 
         for (int i = 0; i < length; i++) {
             System.out.println("Ingrese numero para la posición "+i+": ");
             int nume = myObj.nextInt();
+            num[i]=nume;
             System.out.println("num["+i+"]: "+num[i]);
         }
         System.out.println("\nArray: ");
